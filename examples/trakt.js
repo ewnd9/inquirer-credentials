18ca5c9f706dd13e2e159e78d1b77045e0c04f61
ewnd9 <ewndnine@gmail.com>
2015-11-08 20:28:27 +0300

    add trakt example

---
+++ examples/trakt.js
@@ -0,0 +1,14 @@
+var inquirerCredentials = require('inquirer-credentials');
+
+var appId = '6495';
+
+var pin = {
+  name: 'trakt-token',
+  type: 'input',
+  hint: 'Open "' + 'https://trakt.tv/pin/' + appId + '", then paste pin here',
+  env: 'TRAKT_PIN'
+};
+
+inquirerCredentials('.test', [pin]).then(function(result) {
+  console.log(result);
+});
